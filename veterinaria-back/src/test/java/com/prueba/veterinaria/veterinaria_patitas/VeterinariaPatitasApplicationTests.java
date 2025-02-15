c7554bf09582258bc364d00f0a6f798c2e02c388
Daniel <cruz_dani93@hotmail.com>
2025-02-15 12:05:51 -0600

    initial commit

---
+++ veterinaria-back/src/test/java/com/prueba/veterinaria/veterinaria_patitas/VeterinariaPatitasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.veterinaria.veterinaria_patitas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeterinariaPatitasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
